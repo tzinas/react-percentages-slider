89608eac973d555eee7a6481635d93d5733b1857
tzinas <atzinas2001@gmail.com>
2020-12-10 05:17:51 +0200

    Add example

--- example/src/App.js
+++ example/src/App.js
@@ -1,10 +1,16 @@
-import React from 'react'
+import React, {useState} from 'react'
 
-import { ExampleComponent } from 'react-percentages-slider'
+import PercentagesSlider from 'react-percentages-slider'
 import 'react-percentages-slider/dist/index.css'
 
 const App = () => {
-  return <ExampleComponent text="Create React Library Example 😄" />
+  const [example, setExample] = useState([
+    { text: "Item 1", color:"#e9c46a", percentage: 28 },
+    { text: "Item 2", color: "#e76f51", percentage: 39 },
+    { text: "Item 3", color:"#2a9d8f", percentage: 25 },
+    { text: "Item 4", color:"#2a7c9d", percentage: 17 }])
+
+  return <PercentagesSlider divisions={example} setDivisions={setExample}/>
 }
 
 export default App
